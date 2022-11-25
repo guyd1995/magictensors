e3927c808d6bf435c2ec15684226b5fe18f7ba92
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2022-11-25 13:19:54 +0200

    add counter tracker behavior

--- magictensors/core.py
+++ magictensors/core.py
@@ -6,6 +6,12 @@ from torch import nn
 
 
 ### General Utils
+class MagicShortcutException(Exception):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+
 class _Item:
     def __init__(self, kind, value, has_unique_id, unique_id=None):
         self.kind = kind
@@ -49,12 +55,29 @@ class _ComputationGraph(_Item):
         kwargs = {k: self._get_graph(v) for k, v in kwargs.items()}
         super().__init__(kind=_ComputationGraph.UNIQUE_ID, value=(func, args, kwargs), 
                          has_unique_id=True, unique_id=_ComputationGraph.UNIQUE_ID)
+    
+    def __repr__(self):
+        return self._tree_text(indentation=0)
+        
+    def _tree_text(self, indentation, prefix=None):
+        func, args, kwargs = self.value
+        text = '|' + ('-' * indentation) + (prefix + ': ' if prefix else '') + str(func) + '\n'
+        for cur_list in [zip([None] * len(args), args), kwargs.items()]:
+            for arg_name, arg in cur_list:
+                if isinstance(arg, _ComputationGraph):
+                    text += arg._tree_text(indentation=indentation+1, prefix=arg_name)
+                else: 
+                    text += '|' + ('-' * (indentation + 1)) + str(arg) + '\n'
+        return text
         
     @staticmethod
     def _get_graph(x):
         if is_magic_tensor(x):
             return x._states.get(_ComputationGraph.UNIQUE_ID)
         else:
+            if is_primitive(x):
+                return x
+            
             return None
         
 
@@ -62,6 +85,7 @@ class _ComputationGraph(_Item):
 class _MagicBehavior(_Item):
     FUNC_BEHAVIOR = 'func'
     TRACK_HISTORY = 'track_history'
+    COUNTER = 'counter'
     
     def __iter__(self):
         return iter([self.kind, self.value])
@@ -76,18 +100,28 @@ class _MagicBehavior(_Item):
                 return 'track history: on'
             else: 
                 return ''
+        elif self.kind == _MagicBehavior.COUNTER:
+            name, condition = value
+            return f'counter set: `{str(name)}`'
         else:
             raise NotImplementedError
     
     @staticmethod
     def make_func_behavior(orig_func, new_func):
         return _MagicBehavior(kind=_MagicBehavior.FUNC_BEHAVIOR, value=(orig_func, new_func), 
-                              has_unique_id=True, unique_id=(_MagicBehavior.FUNC_BEHAVIOR, orig_func))
+                              has_unique_id=True, 
+                              unique_id=(_MagicBehavior.FUNC_BEHAVIOR, orig_func))
     
     @staticmethod
-    def make_history_tracker(on):
-        return _MagicBehavior(kind=_MagicBehavior.TRACK_HISTORY, value=on, 
+    def make_history_tracker(enabled):
+        return _MagicBehavior(kind=_MagicBehavior.TRACK_HISTORY, value=enabled, 
                               has_unique_id=True, unique_id=_MagicBehavior.TRACK_HISTORY)
+    
+    @staticmethod
+    def make_counter(name, condition=None):
+        return _MagicBehavior(kind=_MagicBehavior.COUNTER, 
+                              value=(name, condition), has_unique_id=True, 
+                              unique_id=(_MagicBehavior.COUNTER, name))
     
 ### Magic Tensor
 class MagicTensor(torch.Tensor):
@@ -115,8 +149,15 @@ class MagicTensor(torch.Tensor):
     def override_function(self, orig_func, new_func):
         self._behaviors.add(_MagicBehavior.make_func_behavior(orig_func, new_func))
     
-    def track_history(self, on=True):
-        self._behaviors.add(_MagicBehavior.make_history_tracker(on=on))
+    def track_history(self, enabled=True):
+        self._behaviors.add(_MagicBehavior.make_history_tracker(enabled=enabled))
+    
+    def set_counter(self, name, condition=None):
+        behavior = _MagicBehavior.make_counter(name, condition)
+        self._behaviors.add(behavior)
+        self._states.add(_Item(kind=_MagicBehavior.COUNTER, value=0, 
+                                           has_unique_id=True,
+                                           unique_id=(_MagicBehavior.COUNTER, name)))
     
     @classmethod
     def __torch_function__(cls, func, types, args=(), kwargs=None):
@@ -127,7 +168,7 @@ class MagicTensor(torch.Tensor):
         behaviors_list = [a._behaviors if hasattr(a, '_behaviors') else _Pool() for a in args]
         final_states = _Pool()
         
-        for arg_behaviors in behaviors_list:
+        for cur_arg, arg_behaviors in zip(args, behaviors_list):
             for behavior in arg_behaviors:
                 key, value = behavior
                 if key == _MagicBehavior.FUNC_BEHAVIOR:
@@ -135,9 +176,18 @@ class MagicTensor(torch.Tensor):
                     if orig_func == func:
                         func = new_func
                 elif key == _MagicBehavior.TRACK_HISTORY:
-                    on = value
-                    if on:
+                    enabled = value
+                    if enabled:
                         final_states.add(_ComputationGraph(func, args, kwargs))
+                elif key == _MagicBehavior.COUNTER:
+                    name, condition = value
+                    if condition is not None:
+                        raise NotImplementedError("no support for conditional counters right now")
+                    unique_id = (_MagicBehavior.COUNTER, name)
+                    counter_val = cur_arg._states.get(unique_id).value + 1
+                    final_states.add(_Item(kind=_MagicBehavior.COUNTER, value=counter_val, 
+                                           has_unique_id=True,
+                                           unique_id=(_MagicBehavior.COUNTER, name)))
                 else:
                     raise NotImplementedError
 
@@ -162,7 +212,27 @@ class MagicTensor(torch.Tensor):
 
     def item(self):
         return torch.Tensor(self).item()
+
+
+# Magic Modules
+class MagicModule(nn.Module):
+    def __init__(self, module):
+        self._magic_module_inner = module
     
+    def forward(self, *args, **kwargs):
+        try:
+            return self._magic_module_inner.forward(*args, **kwargs)
+        except MagicShortcutException as res:
+            return res.value
+        
+    def __repr__(self):
+        return "MagicModule\n" + self._magic_module_inner.__repr__()
+    
+        
 # more helper functions
+def is_primitive(x):
+    return isinstance(x, (str, bool, int, float, complex))
+
+
 def is_magic_tensor(x):
     return isinstance(x, MagicTensor)
